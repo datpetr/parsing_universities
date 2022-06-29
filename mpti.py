9211d5b76a7ea63297c24d70ff2d595eee9bf5fb
master <dat.petr@gmail.com>
2022-06-29 19:37:40 +0300

    beging_parsing_for_mpti

--- mpti.py
+++ mpti.py
@@ -4,6 +4,5 @@ from bs4 import BeautifulSoup
 url = 'https://pk.mipt.ru/bachelor/list/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('span', class_='text')
-
+quotes = soup.find_all('div', class_='text')
 print(quotes)
