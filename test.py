0aecb4a934576df2231f2e4121d4cdab1b93ed46
master <dat.petr@gmail.com>
2022-06-30 19:07:45 +0300

    beging_parsing_for_mpti

---
+++ test.py
@@ -0,0 +1,27 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://priem.mirea.ru/accepted-entrants-list/'
+HEADS = [
+    {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) '
+                      'Gecko/20100101 Firefox/103.0',
+        'accept': '*/*'
+    }
+]
+
+
+def get_html(url, params=None):
+    return requests.get(url, headers=HEADS, params=params)
+
+
+print(get_html(URL))
+
+
+def parse():
+    html = get_html(URL)
+    print(html.status_code)
+
+
+parse()
