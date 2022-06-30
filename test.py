6436990daa31965fadb7a7ac3d4d56b480dad58d
master <dat.petr@gmail.com>
2022-06-30 16:33:33 +0300

    adding the parsing mirea

--- test.py
+++ test.py
@@ -1,31 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://priem.mirea.ru/accepted-entrants-list/'
-heads = [
+URL = 'https://priem.mirea.ru/accepted-entrants-list/'
+HEADS = [
     {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:76.0) Gecko/20100101 Firefox/76.0',
-        'Accept': '/'
-    },
-    {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0",
-        'Accept': '/'
-    },
-    {
-        "User-Agent": "Mozilla/5.0 (X11; Debian; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0",
-        'Accept': '/'
-    },
-    {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 3.1; rv:76.0) Gecko/20100101 Firefox/69.0',
-        'Accept': '/'
-    },
-    {
-        "User-Agent": "Mozilla/5.0 (X11; Debian; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/76.0",
-        'Accept': '/'
-    },
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) '
+                      'Gecko/20100101 Firefox/103.0',
+        'accept': '*/*'
+    }
 ]
 
 
-response = requests.get(url, headers=heads)
-soup = BeautifulSoup(response.text, 'lxml')
-print(soup.text)
+def get_html(url, params=None):
+    return requests.get(url, headers=HEADS, params=params)
+
+
+def parse():
+    html = get_html(URL)
+    print(html)
+
+
+parse()
