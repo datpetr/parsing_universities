2cea5d75cbdda013582a3d7ce7b9a04ee8b28579
master <dat.petr@gmail.com>
2022-07-04 15:16:43 +0300

    _

--- firefoxdrivers/main.py
+++ firefoxdrivers/main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 
@@ -6,12 +7,15 @@ import time
 url = 'https://lk.abitur.mtuci.ru/staticPage.php?page_name=spiski&ysclid=l56mnhda9t439607663'
 options = webdriver.FirefoxOptions()
 options.add_argument('user-agent=HelloWorld')
-browser = webdriver.Firefox(executable_path='/home/master/PycharmProjects/parsing_universities/firefoxdrivers/geckodriver',
+browser = webdriver.Firefox(executable_path='/home/master/PycharmProjects/'
+                                            'parsing_universities/firefoxdrivers/'
+                                            'geckodriver',
                             options=options)
 
 
 try:
     browser.get(url)
+    browser.find_element(By.LINK_TEXT, 'Информатика и вычислительная техника').click()
     time.sleep(10)
     soup = BeautifulSoup(browser.page_source, 'lxml')
     print(soup)
@@ -20,4 +24,3 @@ except Exception as ex:
 finally:
     browser.close()
     browser.quit()
-
---
+++ mtusi.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+
+
+url = 'https://lk.abitur.mtuci.ru/staticPage.php?page_name=spiski&ysclid=l56mnhda9t439607663'
+
+
+def get_html(url):
+    options = webdriver.FirefoxOptions()
+    options.add_argument('user-agent=HelloWorld')
+    browser = webdriver.Firefox(executable_path='/home/master/PycharmProjects/'
+                                                'parsing_universities/firefoxdrivers/'
+                                                'geckodriver',
+                                options=options)
+
+    try:
+        browser.get(url)
+        browser.find_element(By.LINK_TEXT, 'Информатика и вычислительная техника').click()
+        soup = BeautifulSoup(browser.page_source, 'lxml')
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        browser.close()
+        browser.quit()
